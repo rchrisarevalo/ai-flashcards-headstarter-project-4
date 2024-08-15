8ac78bead85c011f11da82b813f6d20ab32f7ebe
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-15 00:32:56 -0500

    Bug fix

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -8,32 +8,38 @@ const formatAmountForStripe = (amount: number) => {
 };
 
 const POST = async (req: NextRequest) => {
-  const params: Stripe.Checkout.SessionCreateParams = {
-    submit_type: "pay",
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "BrainflashAI Pro Subscription",
-          },
-          unit_amount: formatAmountForStripe(10),
-          recurring: {
-            interval: "month",
-            interval_count: 1,
-          },
-        },
-        quantity: 1,
-      },
-    ],
-    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-  };
-  const checkoutSession: Stripe.Checkout.Session =
-    await stripe.checkout.sessions.create(params);
+  // const params: Stripe.Checkout.SessionCreateParams = {
+  //   submit_type: "pay",
+  //   payment_method_types: ["card"],
+  //   line_items: [
+  //     {
+  //       price_data: {
+  //         currency: "usd",
+  //         product_data: {
+  //           name: "BrainflashAI Pro Subscription",
+  //         },
+  //         unit_amount: formatAmountForStripe(10),
+  //         recurring: {
+  //           interval: "month",
+  //           interval_count: 1,
+  //         },
+  //       },
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+  //   cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+  // };
+  // const checkoutSession: Stripe.Checkout.Session =
+  //   await stripe.checkout.sessions.create(params);
 
-  return NextResponse.json(checkoutSession, {
+  // return NextResponse.json(checkoutSession, {
+  //   status: 200
+  // })
+
+  return NextResponse.json({
+    message: "This works!"
+  }, {
     status: 200
   })
 };
