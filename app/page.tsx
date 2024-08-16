f26136e67e06b9d8758f625f6dddeb79eb1ef729
Mauro Castillo III <mauro.m.castillo3@gmail.com>
2024-08-16 14:23:34 -0500

    Changed link in Nav to route home. Added link to /dashboard and /dashboard/flashcards

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,26 @@
 import { useRouter } from "next/navigation";
 import Navigation from "./components/Nav";
 import { SignOutButton, useAuth } from "@clerk/nextjs";
-import '@fontsource/raleway';
-import '@fontsource/roboto';
+import "@fontsource/raleway";
+import "@fontsource/roboto";
+import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
   const auth = useAuth();
 
   return (
-    <main className="flex min-h-screen bg-gradient-to-t from-[#1476bc] via-[#9fbedb] to-[#ffffff] flex-col items-center justify-center p-10" 
-    style={{ fontFamily: "'Roboto', sans-serif" }}>
+    <main
+      className="flex min-h-screen bg-gradient-to-t from-[#1476bc] via-[#9fbedb] to-[#ffffff] flex-col items-center justify-center p-10"
+      style={{ fontFamily: "'Roboto', sans-serif" }}
+    >
       <Navigation />
       <div className="flex flex-col items-center mt-36 mb-16 space-y-16 w-full max-w-4xl max-sm:w-screen">
         <section className="text-center space-y-5 px-6">
-          <h1 className="text-6xl font-extrabold text-[#1476b]" style={{ fontFamily: "'Raleway', sans-serif" }}>
+          <h1
+            className="text-6xl font-extrabold text-[#1476b]"
+            style={{ fontFamily: "'Raleway', sans-serif" }}
+          >
             BrainflashAI
           </h1>
           <h5 className="text-3xl font-light text-gray-700">
@@ -27,25 +33,28 @@ export default function Home() {
         </section>
         <section className="text-center space-y-5 px-6">
           <p className="text-2xl font-extralight text-gray-600">
-          Meet <b className="text-[#1476bc]" 
-          style={{ fontFamily: "'Raleway', sans-serif" }}>
-            BrainflashAI
-            </b>, 
-          <br></br>
-          the AI-powered flashcard app that 
-          <br></br>
-          transforms your <b>lecture notes</b> into <b>easy-to-use flashcards</b>,
-          <br></br>
-          helping you memorize and understand concepts better!
+            Meet{" "}
+            <b
+              className="text-[#1476bc]"
+              style={{ fontFamily: "'Raleway', sans-serif" }}
+            >
+              BrainflashAI
+            </b>
+            ,<br></br>
+            the AI-powered flashcard app that
+            <br></br>
+            transforms your <b>lecture notes</b> into{" "}
+            <b>easy-to-use flashcards</b>,<br></br>
+            helping you memorize and understand concepts better!
           </p>
         </section>
         <section className="text-center space-y-5 max-w-md mx-auto max-sm:ml-20 max-sm:mr-20">
           <p className="text-2xl font-semibold text-gray-700">
-          Create an account or sign in to start mastering your studies!
+            Create an account or sign in to start mastering your studies!
           </p>
         </section>
         <section className="flex flex-col sm:flex-row items-center font-bold gap-5 justify-center">
-          {!auth?.isSignedIn ?
+          {!auth?.isSignedIn ? (
             <>
               <button
                 className="p-4 sm:p-5 bg-[#1476bc] text-white rounded-lg hover:bg-[#0f5a8b] transition-colors"
@@ -60,17 +69,21 @@ export default function Home() {
                 Sign Up
               </button>
             </>
-            :
+          ) : (
             <SignOutButton />
-          }
+          )}
         </section>
         <section className="flex flex-wrap justify-center gap-5">
-          <figure className="bg-white text-black p-8 rounded-xl shadow-lg transition-transform transform hover:scale-105 w-60 h-60 flex items-center justify-center">
-            <span className="text-xl font-semibold">My Dashboard</span>
-          </figure>
-          <figure className="bg-white text-black p-8 rounded-xl shadow-lg transition-transform transform hover:scale-105 w-60 h-60 flex items-center justify-center">
-            <span className="text-xl font-semibold">Generate New Set</span>
-          </figure>
+          <Link href="/dashboard">
+            <figure className="bg-white text-black p-8 rounded-xl shadow-lg transition-transform transform hover:scale-105 w-60 h-60 flex items-center justify-center">
+              <span className="text-xl font-semibold">My Dashboard</span>
+            </figure>
+          </Link>
+          <Link href="/dashboard/flashcards">
+            <figure className="bg-white text-black p-8 rounded-xl shadow-lg transition-transform transform hover:scale-105 w-60 h-60 flex items-center justify-center">
+              <span className="text-xl font-semibold">Generate New Set</span>
+            </figure>
+          </Link>
         </section>
       </div>
     </main>
